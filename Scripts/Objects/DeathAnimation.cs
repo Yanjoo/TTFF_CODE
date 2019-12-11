c1045ecd034f9f2e2049cc1e9a44c6ec8a706dd6
Yanjoo <43159295+Yanjoo@users.noreply.github.com>
2019-12-11 10:22:37 +0900

    작성자 추가

--- Scripts/Objects/DeathAnimation.cs
+++ Scripts/Objects/DeathAnimation.cs
@@ -1,4 +1,11 @@
-﻿using System.Collections;
+/************************
+ * 프로그램명 : DeathAnimation.cs
+ * 작성자 : 안한길 (이한주, 안한길, 정채은, 황승혜, 홍영선)
+ * 작성일 : 2019년 12월 3일
+ * 프로그램 설명 : badending scene 에서 UI애니메이션을 활성화 한다
+ ************************/
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
