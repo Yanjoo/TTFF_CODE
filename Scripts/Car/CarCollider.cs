1769317101c1359b691d40c6a3b735c8b8c04f6d
Yanjoo <43159295+Yanjoo@users.noreply.github.com>
2019-12-16 03:11:47 +0900

    내용 수정

--- Scripts/Car/CarCollider.cs
+++ Scripts/Car/CarCollider.cs
@@ -1,63 +1,36 @@
-﻿/************************
- * 프로그램명 : CarCollider.cs
- * 작성자 : 황승혜 (이한주, 안한길, 정채은, 황승혜, 홍영선)
+/************************
+ * 프로그램명 : Car.cs
+ * 작성자 : 이한주 (이한주, 안한길, 정채은, 황승혜, 홍영선)
  * 작성일 : 2019년 11월 25일
- * 프로그램 설명 : 자동차의 충돌 관리 후 상태 값 변경
- ************************/ 
+ * 프로그램 설명 : 자동차 객체의 상태 값 관리
+ ************************/
+  
 
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CarCollider : MonoBehaviour
+public class Car : MonoBehaviour
 {
-    private Car target;
-    private Transform firePos;
-    private float SafeDistance;
+    public float MoveSpeed = 1f;
+    public float SafeDistance = 5f;
 
-    // Start is called before the first frame update
+    // 자동차 상태 MOVE STOP STAY
+    public enum CarState { MOVE, STOP, STAY };
+    public CarState state;
+
+    private Rigidbody rigidbody;
+
     void Start()
     {
-        target = GetComponent<Car>();
+        // 자동차 초기 상태
+        state = CarState.MOVE;
+
+        rigidbody = GetComponent<Rigidbody>();
+        // 자동차의 회전축 고정, Y 위치 고정
+        rigidbody.constraints = RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezeRotationX
+            | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationZ;
+        
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        firePos = target.transform;
-        SafeDistance = target.SafeDistance;
-
-        if (target.state == Car.CarState.STAY) target.state = Car.CarState.MOVE;
-
-        Debug.DrawRay(firePos.position, firePos.forward * SafeDistance, Color.green);
-
-        RaycastHit hit;
-        if (Physics.Raycast(firePos.position, firePos.forward, out hit, SafeDistance))
-        {
-            if (hit.collider.tag == "Car")
-            {
-                target.state = Car.CarState.STAY;
-            }
-        }
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        // 자동차가 사라짐
-        if (other.gameObject.CompareTag("Destroy"))
-        {
-            Destroy(this.gameObject);
-        }
-        // 자동차가 멈추고
-        if (other.gameObject.CompareTag("Stop"))
-        {
-            target.state = Car.CarState.STOP;
-        }
-        // 자동차가 출발
-        if (other.gameObject.CompareTag("Go"))
-        {
-            target.state = Car.CarState.MOVE;
-        }
-    }
-
 }
+
