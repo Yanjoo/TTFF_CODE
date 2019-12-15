e2e0a6efa1dd02c1435da78ad48d7e55791ea3dd
Yanjoo <43159295+Yanjoo@users.noreply.github.com>
2019-12-16 03:20:14 +0900

    주석 추가

--- Scripts/Objects/Police.cs
+++ Scripts/Objects/Police.cs
@@ -1,4 +1,4 @@
-﻿/************************
+/************************
  * 프로그램명 : Police.cs
  * 작성자 : 정채은, 홍영선 (이한주, 안한길, 정채은, 황승혜, 홍영선)
  * 작성일 : 2019년 11월 21일
@@ -12,35 +12,41 @@ using UnityEngine.AI;
 
 public class Police : MonoBehaviour
 {
-    private NavMeshAgent nav;
+    // 게임 오브젝트의 Navigation 정보를 분석하여 목표물을 추적하게 하는 컴포넌트
+    private NavMeshAgent nav; 
     private Player player;
     private Vector3 spawnPosition;
 
-    // Start is called before the first frame update
     void Start()
     {
         nav = GetComponent<NavMeshAgent>();
+        // 플레이어를 추적함
         player = GameObject.Find("Player").GetComponent<Player>();
+        // 경찰이 원래 있던 자리
         spawnPosition = transform.position;
     }
 
-    // Update is called once per frame
     void Update()
     {
+        // 플레이어의 상태가 무단횡단이면 추적함
         if (player.IsJaywalking()) Chase();
+        // 아니면 원래 자리로 유지
         else GetBack();
     }
 
+    // 추적하는 함수
     private void Chase()
     {
+        // 자신의 위치와 플레이어의 위치가 다르면 플레이어 위치로 네비게이션 설정
         if (nav.destination != player.transform.position)
             nav.SetDestination(player.transform.position);
-        else
+        else // 네비게이션 위치랑 플레이어 위치랑 똑같을때
             nav.SetDestination(transform.position);
     }
 
     private void GetBack()
     {
+        // 자신의 위치와 원래 위치가 다르면 원래 위치로 네비게이션 설정
         if (nav.destination != spawnPosition)
             nav.SetDestination(spawnPosition);
         else
