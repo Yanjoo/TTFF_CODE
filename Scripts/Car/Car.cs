49dee718b771bbacef9c5aedea0b5df1af57a9c9
Yanjoo <43159295+Yanjoo@users.noreply.github.com>
2019-12-16 03:06:39 +0900

    내용 수정

--- Scripts/Car/Car.cs
+++ Scripts/Car/Car.cs
@@ -1,4 +1,4 @@
-﻿/************************
+/************************
  * 프로그램명 : Car.cs
  * 작성자 : 이한주 (이한주, 안한길, 정채은, 황승혜, 홍영선)
  * 작성일 : 2019년 11월 25일
@@ -15,6 +15,7 @@ public class Car : MonoBehaviour
     public float MoveSpeed = 1f;
     public float SafeDistance = 5f;
 
+    // 자동차 상태 MOVE STOP STAY
     public enum CarState { MOVE, STOP, STAY };
     public CarState state;
 
@@ -22,11 +23,13 @@ public class Car : MonoBehaviour
 
     void Start()
     {
+        // 자동차 초기 상태
         state = CarState.MOVE;
 
         rigidbody = GetComponent<Rigidbody>();
-        rigidbody.constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezePositionZ
-            | RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationZ;
+        // 자동차의 회전축 고정, Y 위치 고정
+        rigidbody.constraints = RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezeRotationX
+            | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationZ;
         
     }
 }
