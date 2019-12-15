01897477924f57630f00aa9c81a7521543727dab
Yanjoo <43159295+Yanjoo@users.noreply.github.com>
2019-12-16 03:12:22 +0900

    내용 수정

--- Scripts/Car/CarMove.cs
+++ Scripts/Car/CarMove.cs
@@ -1,8 +1,8 @@
-﻿/************************
- * 프로그램명 : CarMove.cs
- * 작성자 : 정채은, 홍영선 (이한주, 안한길, 정채은, 황승혜, 홍영선)
+/************************
+ * 프로그램명 : Car.cs
+ * 작성자 : 이한주 (이한주, 안한길, 정채은, 황승혜, 홍영선)
  * 작성일 : 2019년 11월 25일
- * 프로그램 설명 : 자동차의 상태 값에 따라서 자동차 이동 구현
+ * 프로그램 설명 : 자동차 객체의 상태 값 관리
  ************************/
   
 
@@ -10,25 +10,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CarMove : MonoBehaviour
+public class Car : MonoBehaviour
 {
-    private Car.CarState state;
-    private Car target;
-    private float targetSpeed;
+    public float MoveSpeed = 1f;
+    public float SafeDistance = 5f;
 
-    // Start is called before the first frame update
+    // 자동차 상태 MOVE STOP STAY
+    public enum CarState { MOVE, STOP, STAY };
+    public CarState state;
+
+    private Rigidbody rigidbody;
+
     void Start()
     {
-        target = GetComponent<Car>();
-    }
+        // 자동차 초기 상태
+        state = CarState.MOVE;
 
-    // Update is called once per frame
-    void Update()
-    {
-        state = target.state;
-        targetSpeed = target.MoveSpeed;
-
-        if (state == Car.CarState.MOVE)
-            transform.Translate(0f, 0f, targetSpeed * Time.deltaTime); // 차 이동
+        rigidbody = GetComponent<Rigidbody>();
+        // 자동차의 회전축 고정, Y 위치 고정
+        rigidbody.constraints = RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezeRotationX
+            | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationZ;
+        
     }
 }
+
